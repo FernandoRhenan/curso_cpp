de7185e03c1fc100d13b06f1b7f49a605bc7c149
FernandoRhenan <fernandorhenan9@gmail.com>
2023-11-28 22:00:47 -0300

    i/o terminal

--- aulas/aula3.cpp
+++ aulas/aula3.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main(){
 
+    int numero;
+
     cout << "Testando...\n";
+    cout << "Digite um numero:\n";
+    cin >> numero;
+    cout << "Seu numero: " << numero << "\n";
+
 
     system("pause");
     return 0;
