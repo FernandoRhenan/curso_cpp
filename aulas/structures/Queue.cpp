e0b6d83999d9a323b4f395acc8cee4c9a5ca42bb
FernandoRhenan <fernandorhenan9@gmail.com>
2023-12-19 14:54:42 -0300

    Queue

--- aulas/structures/Queue.cpp
+++ aulas/structures/Queue.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <stack>
+#include <queue>
 
 using namespace std;
 
 int main()
 {
 
-    stack<string> cartas;
+    queue<string> cartas;
 
     cartas.push("Rei de Espadas");
     cartas.push("Rei de Copas");
     cartas.push("Rei de Ouro");
     cartas.push("Rei de Paus");
 
-    cout << "Proximo a remover: " << cartas.top() << "\n";
+    cout << "Primeiro da fila: " << cartas.front() << "\n";
+    cout << "Ultimo da fila da fila: " << cartas.back() << "\n";
     cout << "Comprimento do conjunto: " << cartas.size() << "\n";
-
     return 0;
 }
