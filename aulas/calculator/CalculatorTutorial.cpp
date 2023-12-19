698e03b6a4658849580ab4b450c5f8bc3d076042
FernandoRhenan <fernandorhenan9@gmail.com>
2023-12-18 21:13:40 -0300

    Calculator

---
+++ aulas/calculator/CalculatorTutorial.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+
+    double x = 0.0;
+    double y = 0.0;
+    char oper = '+';
+    double result = 0.0;
+
+    cout << "Calculator Console Application" << endl
+         << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+         << endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "Attempted to divide by zero!" << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Result "
+             << "of " << x << oper << y << " is: " << result << endl;
+    };
+
+    return 0;
+}
